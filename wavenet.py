f1163516972ea4342c52f8a99d3f386ed8efe1b8
Zhou Zheng <204784224@qq.com>
2023-10-22 23:21:40 +0800

    多头现在可以跑通

--- wavenet.py
+++ wavenet.py
@@ -25,6 +25,43 @@ class TCNBlock(nn.Module):
         return x.transpose(1, 2)
 
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, embed_size, heads):
+        super(MultiHeadAttention, self).__init__()
+        self.embed_size = embed_size
+        self.heads = heads
+        self.head_dim = embed_size // heads
+
+        assert self.head_dim * heads == embed_size, "Embedding size needs to be divisible by heads"
+
+        self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.fc_out = nn.Linear(heads * self.head_dim, embed_size)
+
+    def forward(self, values, keys, query):
+        N = query.shape[0]
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+
+        # Split the embedding into self.heads different pieces
+        values = values.reshape(N, value_len, self.heads, self.head_dim)
+        keys = keys.reshape(N, key_len, self.heads, self.head_dim)
+        queries = query.reshape(N, query_len, self.heads, self.head_dim)
+
+        values = self.values(values)
+        keys = self.keys(keys)
+        queries = self.queries(queries)
+
+        # Scaled dot-product attention
+        inner_product = torch.einsum("nqhd,nkhd->nhqk", [queries, keys])
+        keys_dim = torch.tensor(self.head_dim, dtype=torch.float32, requires_grad=False)
+        scaled_inner_product = inner_product / torch.sqrt(keys_dim)
+        attention = torch.nn.functional.softmax(scaled_inner_product, dim=3)
+        out = torch.einsum("nhql,nlhd->nqhd", [attention, values]).reshape(N, query_len, self.heads * self.head_dim)
+
+        return self.fc_out(out)
+
+
 class WaveNetBlock(nn.Module):
     def __init__(self, input_dim, output_dim, kernel_size=2, dilation=1):
         super(WaveNetBlock, self).__init__()
@@ -49,6 +86,9 @@ class CombinedModel(nn.Module):
         # TCN层
         self.tcn = TCNBlock(input_dim, hidden_dim)
 
+        # Attention层
+        self.multi_head_attention = MultiHeadAttention(embed_size=hidden_dim, heads=8)
+
         # WaveNet层
         self.wavenet = WaveNetBlock(hidden_dim, hidden_dim)
 
@@ -60,6 +100,7 @@ class CombinedModel(nn.Module):
 
     def forward(self, x):
         x = self.tcn(x)
+        x = self.multi_head_attention(x, x, x)
         x = self.wavenet(x)
         x, _ = self.lstm(x)
         x = self.fc(x[:, -1, :])
